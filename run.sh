0b1bd1137f4679130c58e4daf59e312b0207ed96
robinyen <robingraceily@gmail.com>
2017-04-25 21:00:47 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -11,22 +11,30 @@
 
 #./darknet detector train cfg/sea/sea-b64.data  cfg/sea/sea-b64.cfg
 
-./darknet detector train cfg/sea/sea-b64.data  cfg/sea/sea-b64.cfg backup/sea-b64_2000.weights
+#./darknet detector train cfg/sea/sea-b64.data  cfg/sea/sea-b64.cfg backup/sea-b64_2000.weights
 
 #
 
 ##----------------------------Train 32b 64b on New from scratch
 
 
-./darknet detector train cfg/sea/sea-b64w.data  cfg/sea/sea-b64w.cfg
 ./darknet detector train cfg/sea/sea-b32w.data  cfg/sea/sea-b32w.cfg
 
 
+./darknet detector train cfg/sea/sea-a32w.data  cfg/sea/sea-a32w.cfg
 
+./darknet detector train cfg/sea/sea-a16w.data  cfg/sea/sea-a16w.cfg
+
+
+./darknet detector train cfg/sea/sea-b64w.data  cfg/sea/sea-b64w.cfg backup/sea-b64w_4400.weights
+
+
+./darknet detector test cfg/sea/sea-b64w.data  cfg/sea/sea-b64w.cfg backup/sea-b64w_4000.weights 
+./darknet detector test cfg/sea/sea-a32w.data  cfg/sea/sea-a32w.cfg backup/sea-b32w_4400.weights 
 
 #test  ------------------------------------
 
-#./darknet detector test cfg/sea/sea-b64w.data  cfg/sea/sea-b64.cfg backup/sea-b64_2000.weights 
+#./darknet detector test cfg/sea/sea-b64w.data  cfg/sea/sea-b64w.cfg backup/sea-b64w_2000.weights 
 
 
 
@@ -36,3 +44,7 @@
 #./darknet detector recall cfg/sea/Basic_32.data  cfg/sea/sea-b32.cfg backup/sea-b32_4400.weights -out log_recall_b32.txt
 #./darknet detector recall cfg/sea/Basic_64.data  cfg/sea/sea-b64.cfg backup/sea-b64_900.weights -out log_recall_b64_900.txt
 
+
+# valid
+
+./darknet detector valid cfg/sea/sea-a32w.data  cfg/sea/sea-a32w.cfg backup/sea-a2w_4400.weights 
